175034e8d3ea1f0be0325f621cb7edf9157c215a
Mike Nolta <mike@nolta.net>
2017-10-31 10:34:45 -0400

    handle graphviz filter errors

--- filters/graphviz.py
+++ filters/graphviz.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
 
-import panflute
 import subprocess
+import sys
 import tempfile
+
+def err(msg):
+    sys.stderr.write("markdown2pdf/filters/graphviz: {}\n".format(msg))
 
 def graphviz(elem, doc):
     if type(elem) == panflute.CodeBlock and 'dot' in elem.classes:
         name = tempfile.mktemp(suffix='.pdf')
         args = ['dot', '-Tpdf', '-o', name]
-        dot = subprocess.run(args, input=elem.text.encode('utf8'),
-                stdout=subprocess.DEVNULL)
-        if dot.returncode == 0:
-            img = panflute.Image(url=name)
-            return panflute.Para(img)
+        try:
+            dot = subprocess.run(args, input=elem.text.encode('utf8'),
+                    stdout=subprocess.DEVNULL)
+            if dot.returncode == 0:
+                img = panflute.Image(url=name)
+                return panflute.Para(img)
+        except:
+            err("error running dot")
 
 def main(doc=None):
     panflute.run_filter(graphviz, doc=doc)
 
 if __name__ == '__main__':
-    main()
+    try:
+        import panflute
+        main()
+    except:
+        err("can't import panflute")
+        sys.stdout.write(sys.stdin.read())
+
