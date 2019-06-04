8f6940ac3cf8d4d70f17f74bfb7fe369e1648d6a
Mike Nolta <mike@nolta.net>
2019-06-04 21:12:42 +0000

    handle metapost diagrams

--- filters/diagrams.py
+++ filters/diagrams.py
@@ -8,20 +8,22 @@ import tempfile
 def err(msg):
     sys.stderr.write("markdown2pdf/filters/diagrams: {}\n".format(msg))
 
+def extract_code_to_tmpfile_with_ext(elem, ext):
+    if ext.startswith('.'):
+        suffix = ext
+    else:
+        suffix = '.' + ext
+    tmp = tempfile.NamedTemporaryFile(suffix=suffix, delete=False)
+    txt = elem.text.encode('utf-8')
+    tmp.write(txt)
+    tmp.close()
+    return tmp
+
 def diagrams(elem, doc):
     if type(elem) != panflute.CodeBlock:
         return
     if 'dot' in elem.classes:
-        if "filename" in elem.attributes:
-            if not os.path.exists(os.path.dirname(elem.attributes["filename"])):
-                os.makedirs(os.path.dirname(elem.attributes["filename"]))
-            tmp = open(elem.attributes["filename"],'wb')
-        else:
-            tmp = tempfile.NamedTemporaryFile(suffix=".txt", delete=False)
-        txt = elem.text.encode('utf-8')
-        tmp.write(txt)
-        tmp.close()
-        #
+        tmp = extract_code_to_tmpfile_with_ext(elem, 'dot')
         ifn = tmp.name
         ifn_without_ext = os.path.splitext(ifn)[0]
         iofn_svg = ifn_without_ext + '.svg'
@@ -43,13 +45,25 @@ def diagrams(elem, doc):
                 '-T'+export_type, ifn,
                 '-o', ofn]
         try:
-            dot = subprocess.check_call(args)
-            if dot != 0:
-                err("error running dot")
-                return
+            subprocess.check_call(args)
         except Exception as ex:
             err("error running dot")
             err(ex)
+            return
+
+    elif 'metapost' in elem.classes:
+        f = extract_code_to_tmpfile_with_ext(elem, 'mp')
+        ifn = f.name
+        ofn = ifn[:-3] + '-1.pdf'
+        try:
+            subprocess.check_output(['mptopdf', ifn],
+                    cwd=os.path.dirname(ifn),
+                    stdin=subprocess.DEVNULL)
+        except subprocess.CalledProcessError as ex:
+            err("error running metapost")
+            err(ex)
+            err(ex.output)
+            return
 
     elif 'plantuml' in elem.classes:
         if '@startuml' in elem.text:
@@ -73,7 +87,7 @@ def diagrams(elem, doc):
             txt = b"@startuml\n" + txt + b"\n@enduml\n"
         tmp.write(txt)
         tmp.close()
-        #
+
         ifn = tmp.name
         ifn_without_ext = os.path.splitext(ifn)[0]
         iofn_svg = ifn_without_ext + '.svg'
@@ -84,11 +98,14 @@ def diagrams(elem, doc):
                 '-charset', 'UTF-8',
                 '-tsvg',
                 ifn] + extra_args
-        #err(" ".join(args))
-        #
-        plantuml = subprocess.check_call(args)
-        if plantuml != 0:
+
+        try:
+            subprocess.check_call(args)
+        except Exception as ex:
+            err("error running plantuml")
+            err(ex)
             return
+
         if doc.format in ["odt","docx"]:
             ofn = iofn_svg
         else:
@@ -97,24 +114,23 @@ def diagrams(elem, doc):
                     '-f', 'pdf',
                     '-o', ofn,
                     iofn_svg]
-            rsvg_convert = subprocess.check_call(args,
-                        stdin=subprocess.DEVNULL,
-                        stdout=subprocess.DEVNULL)
+            subprocess.check_call(args,
+                    stdin=subprocess.DEVNULL,
+                    stdout=subprocess.DEVNULL)
     else:
         return
-    #
-    additional_attributes = []
-    for key,value in elem.attributes.items():
-        if key=='caption':
-            additional_attributes.append(panflute.Str(value))
-        elif key=='cross_ref':
-            #
-            additional_attributes.append(panflute.RawInline("\\label{{{0}}}".format(value), format="latex"))
-    if "caption" in elem.attributes:
-        img = panflute.Image(*additional_attributes,url=ofn, title="fig:")
-    else:
-        img = panflute.Image(url=ofn)
 
+    img_args = []
+    img_opts = dict(url=ofn)
+    for key,value in elem.attributes.items():
+        if key == 'caption':
+            img_args.append(panflute.Str(value))
+            # strange, caption arg is ignored unless title is set
+            img_opts['title'] = 'fig:'
+        elif key == 'cross_ref':
+            img_args.append(panflute.RawInline(
+                "\\label{{{0}}}".format(value), format="latex"))
+    img = panflute.Image(*img_args, **img_opts)
     return panflute.Para(img)
 
 def main(doc=None):
@@ -123,7 +139,7 @@ def main(doc=None):
 if __name__ == '__main__':
     try:
         import panflute
-    except:
-        err("can't import panflute")
+    except Exception as ex:
+        err(ex)
         sys.stdout.write(sys.stdin.read())
-    main()        
+    main()
